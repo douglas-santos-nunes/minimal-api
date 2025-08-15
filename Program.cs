50e5c99ec6b3431c7aae6fc502f165fd1d909e6f
Douglas Nunes <douglasnunesprogramador@gmail.com>
2025-08-14 22:41:52 -0300

    Configurações iniciais

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("mysql"),
+ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
